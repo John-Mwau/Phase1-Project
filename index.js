7511e51f7d46187931ddd0185c666262eb6a6537
john <jmulimwau@gmail.com>
2023-10-26 11:04:55 +0300

    Add Event Listener

--- index.js
+++ index.js
@@ -9,11 +9,12 @@
     const appKey = "329155bd64ad8ebb0f33a82ffc58e501";
   
     form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      searchQuery = e.target.querySelector("input").value;
-      getAPIData();
-    });
-  
+       e.preventDefault();
+       searchQuery = e. target. querySelector ("input"). value;
+       getAPIData();
+      
+    })
+    
     const getAPIData = async () => {
       //Base url containing user query,appId & appKey
       const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${appId}&app_key=${appKey}&from=0&to=45`;
@@ -42,29 +43,5 @@
       })
       displayResultDiv.innerHTML=createdHTML;
     }
-    /*
-    const createHTML = (results) => {
-      let createdHTML = "";
-      results.map((result) => {
-        createdHTML += `
-        <div class="item">
-          <img src="${result.recipe.image}" alt="img"> 
-          <div class="flex-container">
-            <h1 class="title">${result.recipe.label}</h1>
-            <a class="checkout-btn" target="_top" href="${
-              result.recipe.url
-            }">Checkout Recipe</a>
-          </div>
-          <p class="item-data">Cuisine type: ${result.recipe.cuisineType}</p>
-          <p class="item-data">Health labels: ${result.recipe.healthLabels}</p>
-        </div>
-      `;
-      });
-      displayResultDiv.innerHTML = createdHTML;
-    };
-    */
   });
-function dispalyitems (){
-  let y=10;
-}
   
